298dea22eda25b4c600cf2bc4ca78c5eb98bdae2
spacewave <wladimir.nabok@haw-hamburg.de>
2020-10-15 00:40:33 +0200

    WIP OpenGl

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
-#include "opencv2/opencv.hpp"
-using namespace cv;
-int main(int argc, char** argv)
-{
-    VideoCapture cap;
-    // open the default camera, use something different from 0 otherwise;
-    // Check VideoCapture documentation.
-    if(!cap.open(0))
-        return 0;
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame;
-        if( frame.empty() ) break; // end of video stream
-        imshow("this is you, smile! :)", frame);
-        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
-    }
-    // the camera will be closed automatically upon exit
-    // cap.close();
+#include <iostream>
+#include <stdio.h>
+#include <GL/glut.h>
+
+
+int main(int argc, char** argv) {
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("red 3D lighted cube");
+    printf("GL_VERSION = %s\n",glGetString(GL_VERSION) ); /* GL_VERSION = 2.1.2 NVIDIA 195.36.24 */
     return 0;
 }
