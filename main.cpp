d861f6e88c315954a5e4f68c3467a547fb8159a4
spacewave <wladimir.nabok@haw-hamburg.de>
2020-10-26 14:28:03 +0100

    Empty window with glut

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <stdio.h>
-#include <GL/glut.h>
+#include <gl/glut.h>
+
+void display (void) {
+    //glClearColor (1.0, 1.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+   // gluLookAt(0.0,0.5,0,0,0,0,0,1,0);
+    glFlush();
+}
+
+void Keyboard (unsigned char key, int x, int y) {
+    if(key == 27) {
+        exit(0);
+    }   else printf("Sie haben %c gedrueckt, \n", key);
+}
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
 
 
-int main(int argc, char** argv) {
-
+int main (int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("red 3D lighted cube");
-    printf("GL_VERSION = %s\n",glGetString(GL_VERSION) ); /* GL_VERSION = 2.1.2 NVIDIA 195.36.24 */
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("Beispiel: Fenster");
+    glutDisplayFunc(display);
+    glutKeyboardFunc(Keyboard);
+    printf("GL_VERSION = %s\n",glGetString(GL_VERSION) );
+
+    glutMainLoop();
+
     return 0;
 }
