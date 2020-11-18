a65645110a83c20acdaa97f57719e9bab88ef76a
spacewave <wladimir.nabok@haw-hamburg.de>
2020-11-18 04:39:04 +0100

    Send Json joint data with websockets functional

---
+++ kinect-api/src/kinect_websocket.cpp
@@ -0,0 +1,44 @@
+#include <websocketpp/config/core.hpp>
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+#include <iostream>
+#include <fstream>
+#include "kinect_websocket.h"
+#pragma comment(lib,"Ws2_32.lib")
+
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+void on_message(websocketpp::connection_hdl conn, server::message_ptr msg);
+server print_server;
+std::string *test;
+
+kinect_websocket::kinect_websocket() {
+
+    print_server.set_message_handler(&on_message);
+    print_server.set_access_channels(websocketpp::log::alevel::all);
+    print_server.set_error_channels(websocketpp::log::elevel::all);
+
+    print_server.init_asio();
+    print_server.listen(9002);
+    print_server.start_accept();
+
+    std::cout << "Running Server" << std::endl;
+}
+
+
+void kinect_websocket::run() {
+    print_server.run();
+}
+
+void kinect_websocket::setJson(std::string *json) {
+    test = json;
+}
+
+void on_message(websocketpp::connection_hdl conn, server::message_ptr msg) {
+    std::cout << msg->get_payload() << std::endl;
+    std::cout << *test << std::endl;
+
+    print_server.send(conn,*test,websocketpp::frame::opcode::TEXT);
+}
--- kinect-api/src/main.cpp
+++ kinect-api/src/main.cpp
@@ -1,19 +1,28 @@
+
+#define WIN32_LEAN_AND_MEAN
 #include "main.h"
 #include "glut.h"
+
 
 #include <cmath>
 #include <cstdio>
 
-#include <Windows.h>
-#include <Ole2.h>
 
-#include <iostream> 
+#include "kinect_websocket.h"
+#include <Ole2.h>
+#include <ws2def.h>
+
+#include <json/json.h>
+
+#include <iostream>
 #include <NuiApi.h>
 #include <NuiImageCamera.h>
 #include <NuiSensor.h>
+#include <thread>
+
 
 // OpenGL Variables
-long depthToRgbMap[width*height*2];
+long depthToRgbMap[width * height * 2];
 // We'll be using buffer objects to store the kinect point cloud
 GLuint vboId;
 GLuint cboId;
@@ -21,10 +30,15 @@ GLuint cboId;
 // Kinect variables
 HANDLE depthStream;
 HANDLE rgbStream;
-INuiSensor* sensor;
+INuiSensor *sensor;
+
+int frame = 0;
 
 // Stores the coordinates of each joint
 Vector4 skeletonPosition[NUI_SKELETON_POSITION_COUNT];
+kinect_websocket *kinectWebsocketPointer;
+
+std::string jsonData;
 
 bool initKinect() {
     // Get a working kinect sensor
@@ -33,205 +47,255 @@ bool initKinect() {
     if (NuiCreateSensorByIndex(0, &sensor) < 0) return false;
 
     // Initialize sensor
-    sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH_AND_PLAYER_INDEX | NUI_INITIALIZE_FLAG_USES_COLOR | NUI_INITIALIZE_FLAG_USES_SKELETON);
+    sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH_AND_PLAYER_INDEX | NUI_INITIALIZE_FLAG_USES_COLOR |
+                          NUI_INITIALIZE_FLAG_USES_SKELETON);
     sensor->NuiImageStreamOpen(NUI_IMAGE_TYPE_DEPTH_AND_PLAYER_INDEX, // Depth camera or rgb camera?
-        NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
-        0,        // Image stream flags, e.g. near mode
-        2,        // Number of frames to buffer
-        NULL,     // Event handle
-        &depthStream);
-	sensor->NuiImageStreamOpen(NUI_IMAGE_TYPE_COLOR, // Depth camera or rgb camera?
-        NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
-        0,      // Image stream flags, e.g. near mode
-        2,      // Number of frames to buffer
-        NULL,   // Event handle
-		&rgbStream);
-	sensor->NuiSkeletonTrackingEnable(NULL, 0); // NUI_SKELETON_TRACKING_FLAG_ENABLE_SEATED_SUPPORT for only upper body
+                               NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
+                               0,        // Image stream flags, e.g. near mode
+                               2,        // Number of frames to buffer
+                               NULL,     // Event handle
+                               &depthStream);
+    sensor->NuiImageStreamOpen(NUI_IMAGE_TYPE_COLOR, // Depth camera or rgb camera?
+                               NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
+                               0,      // Image stream flags, e.g. near mode
+                               2,      // Number of frames to buffer
+                               NULL,   // Event handle
+                               &rgbStream);
+    sensor->NuiSkeletonTrackingEnable(NULL, 0); // NUI_SKELETON_TRACKING_FLAG_ENABLE_SEATED_SUPPORT for only upper body
     return sensor;
 }
 
-void getDepthData(GLubyte* dest) {
-	float* fdest = (float*) dest;
-	long* depth2rgb = (long*) depthToRgbMap;
+void getDepthData(GLubyte *dest) {
+    float *fdest = (float *) dest;
+    long *depth2rgb = (long *) depthToRgbMap;
     NUI_IMAGE_FRAME imageFrame;
     NUI_LOCKED_RECT LockedRect;
     if (sensor->NuiImageStreamGetNextFrame(depthStream, 0, &imageFrame) < 0) return;
-    INuiFrameTexture* texture = imageFrame.pFrameTexture;
+    INuiFrameTexture *texture = imageFrame.pFrameTexture;
     texture->LockRect(0, &LockedRect, NULL, 0);
     if (LockedRect.Pitch != 0) {
-        const USHORT* curr = (const USHORT*) LockedRect.pBits;
+        const USHORT *curr = (const USHORT *) LockedRect.pBits;
         for (int j = 0; j < height; ++j) {
-			for (int i = 0; i < width; ++i) {
-				// Get depth of pixel in millimeters
-				USHORT depth = NuiDepthPixelToDepth(*curr++);
-				// Store coordinates of the point corresponding to this pixel
-				Vector4 pos = NuiTransformDepthImageToSkeleton(i, j, depth<<3, NUI_IMAGE_RESOLUTION_640x480);
-				*fdest++ = pos.x/pos.w;
-				*fdest++ = pos.y/pos.w;
-				*fdest++ = pos.z/pos.w;
-				// Store the index into the color array corresponding to this pixel
-				NuiImageGetColorPixelCoordinatesFromDepthPixelAtResolution(
-					NUI_IMAGE_RESOLUTION_640x480, NUI_IMAGE_RESOLUTION_640x480, NULL,
-					i, j, depth<<3, depth2rgb, depth2rgb+1);
-				depth2rgb += 2;
-			}
-		}
+            for (int i = 0; i < width; ++i) {
+                // Get depth of pixel in millimeters
+                USHORT depth = NuiDepthPixelToDepth(*curr++);
+                // Store coordinates of the point corresponding to this pixel
+                Vector4 pos = NuiTransformDepthImageToSkeleton(i, j, depth << 3, NUI_IMAGE_RESOLUTION_640x480);
+                *fdest++ = pos.x / pos.w;
+                *fdest++ = pos.y / pos.w;
+                *fdest++ = pos.z / pos.w;
+                // Store the index into the color array corresponding to this pixel
+                NuiImageGetColorPixelCoordinatesFromDepthPixelAtResolution(
+                        NUI_IMAGE_RESOLUTION_640x480, NUI_IMAGE_RESOLUTION_640x480, NULL,
+                        i, j, depth << 3, depth2rgb, depth2rgb + 1);
+                depth2rgb += 2;
+            }
+        }
     }
     texture->UnlockRect(0);
     sensor->NuiImageStreamReleaseFrame(depthStream, &imageFrame);
 }
 
-void getRgbData(GLubyte* dest) {
-	float* fdest = (float*) dest;
-	long* depth2rgb = (long*) depthToRgbMap;
-	NUI_IMAGE_FRAME imageFrame;
+void getRgbData(GLubyte *dest) {
+    float *fdest = (float *) dest;
+    long *depth2rgb = (long *) depthToRgbMap;
+    NUI_IMAGE_FRAME imageFrame;
     NUI_LOCKED_RECT LockedRect;
     if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0) return;
-    INuiFrameTexture* texture = imageFrame.pFrameTexture;
+    INuiFrameTexture *texture = imageFrame.pFrameTexture;
     texture->LockRect(0, &LockedRect, NULL, 0);
     if (LockedRect.Pitch != 0) {
-        const BYTE* start = (const BYTE*) LockedRect.pBits;
+        const BYTE *start = (const BYTE *) LockedRect.pBits;
         for (int j = 0; j < height; ++j) {
-			for (int i = 0; i < width; ++i) {
-				// Determine rgb color for each depth pixel
-				long x = *depth2rgb++;
-				long y = *depth2rgb++;
-				// If out of bounds, then don't color it at all
-				if (x < 0 || y < 0 || x > width || y > height) {
-					for (int n = 0; n < 3; ++n) *(fdest++) = 0.0f;
-				}
-				else {
-					const BYTE* curr = start + (x + width*y)*4;
-					for (int n = 0; n < 3; ++n) *(fdest++) = curr[2-n]/255.0f;
-				}
+            for (int i = 0; i < width; ++i) {
+                // Determine rgb color for each depth pixel
+                long x = *depth2rgb++;
+                long y = *depth2rgb++;
+                // If out of bounds, then don't color it at all
+                if (x < 0 || y < 0 || x > width || y > height) {
+                    for (int n = 0; n < 3; ++n) *(fdest++) = 0.0f;
+                } else {
+                    const BYTE *curr = start + (x + width * y) * 4;
+                    for (int n = 0; n < 3; ++n) *(fdest++) = curr[2 - n] / 255.0f;
+                }
 
-			}
-		}
+            }
+        }
     }
     texture->UnlockRect(0);
     sensor->NuiImageStreamReleaseFrame(rgbStream, &imageFrame);
 }
 
 void getSkeletalData() {
-	NUI_SKELETON_FRAME skeletonFrame = {0};
+    NUI_SKELETON_FRAME skeletonFrame = {0};
     if (sensor->NuiSkeletonGetNextFrame(0, &skeletonFrame) >= 0) {
-		sensor->NuiTransformSmooth(&skeletonFrame, NULL);
-		// Loop over all sensed skeletons
-		for (int z = 0; z < NUI_SKELETON_COUNT; ++z) {
-			const NUI_SKELETON_DATA& skeleton = skeletonFrame.SkeletonData[z];
-			// Check the state of the skeleton
-			if (skeleton.eTrackingState == NUI_SKELETON_TRACKED) {
-			    std::cout << "Found Skeleton" << std::endl;
-				// Copy the joint positions into our array
-				for (int i = 0; i < NUI_SKELETON_POSITION_COUNT; ++i) {
-					skeletonPosition[i] = skeleton.SkeletonPositions[i];
-					if (skeleton.eSkeletonPositionTrackingState[i] == NUI_SKELETON_POSITION_NOT_TRACKED) {
-						skeletonPosition[i].w = 0;
-					}
-				}
-				return; // Only take the data for one skeleton
-			}
-		}
-	}
+        sensor->NuiTransformSmooth(&skeletonFrame, NULL);
+        // Loop over all sensed skeletons
+        for (int z = 0; z < NUI_SKELETON_COUNT; ++z) {
+            const NUI_SKELETON_DATA &skeleton = skeletonFrame.SkeletonData[z];
+            // Check the state of the skeleton
+            if (skeleton.eTrackingState == NUI_SKELETON_TRACKED) {
+                // Copy the joint positions into our array
+                for (int i = 0; i < NUI_SKELETON_POSITION_COUNT; ++i) {
+                    skeletonPosition[i] = skeleton.SkeletonPositions[i];
+                    std::cout << "[" << i << "](" << skeletonPosition[i].x << "," << skeletonPosition[i].y << ","
+                              << skeletonPosition[i].z << ")" << std::endl;
+                    if (skeleton.eSkeletonPositionTrackingState[i] == NUI_SKELETON_POSITION_NOT_TRACKED) {
+                        skeletonPosition[i].w = 0;
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
 }
+
 void getKinectData() {
-	const int dataSize = width*height*3*4;
-	GLubyte* ptr;
-	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	ptr = (GLubyte*) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-	if (ptr) {
-		getDepthData(ptr);
-	}
-	glUnmapBuffer(GL_ARRAY_BUFFER);
-	glBindBuffer(GL_ARRAY_BUFFER, cboId);
-	ptr = (GLubyte*) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-	if (ptr) {
-		getRgbData(ptr);
-	}
-	glUnmapBuffer(GL_ARRAY_BUFFER);
-	getSkeletalData();
+    const int dataSize = width * height * 3 * 4;
+    GLubyte *ptr;
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    ptr = (GLubyte *) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+    if (ptr) {
+        getDepthData(ptr);
+    }
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+    glBindBuffer(GL_ARRAY_BUFFER, cboId);
+    ptr = (GLubyte *) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+    if (ptr) {
+        getRgbData(ptr);
+    }
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+    getSkeletalData();
 }
 
 void rotateCamera() {
-	static double angle = 0.;
-	static double radius = 3.;
-	double x = radius*sin(angle);
-	double z = radius*(1-cos(angle)) - radius/2;
-	glMatrixMode(GL_MODELVIEW);
+    static double angle = 0.;
+    static double radius = 3.;
+    double x = radius * sin(angle);
+    double z = radius * (1 - cos(angle)) - radius / 2;
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-	gluLookAt(x,0,z,0,0,radius/2,0,1,0);
-	angle += 0.05;
+    gluLookAt(x, 0, z, 0, 0, radius / 2, 0, 1, 0);
+    angle += 0.05;
+}
+
+
+Json::Value fillJoint(Json::Value json, std::string jointIdentifier, Vector4 input) {
+    json[jointIdentifier]["x"] = input.x;
+    json[jointIdentifier]["y"] = input.y;
+    json[jointIdentifier]["z"] = input.z;
+    json[jointIdentifier]["w"] = input.w;
+    return json;
 }
 
 void drawKinectData() {
-	getKinectData();
-	rotateCamera();
+    getKinectData();
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	glVertexPointer(3, GL_FLOAT, 0, NULL);
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glVertexPointer(3, GL_FLOAT, 0, NULL);
 
-	glBindBuffer(GL_ARRAY_BUFFER, cboId);
-	glColorPointer(3, GL_FLOAT, 0, NULL);
+    glBindBuffer(GL_ARRAY_BUFFER, cboId);
+    glColorPointer(3, GL_FLOAT, 0, NULL);
 
-	glPointSize(1.f);
-	glDrawArrays(GL_POINTS, 0, width*height);
+    glPointSize(1.f);
+    glDrawArrays(GL_POINTS, 0, width * height);
 
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
 
-	// Draw some arms
-	const Vector4& lh = skeletonPosition[NUI_SKELETON_POSITION_HAND_LEFT];
-	const Vector4& le = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_LEFT];
-	const Vector4& ls = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_LEFT];
-	const Vector4& rh = skeletonPosition[NUI_SKELETON_POSITION_HAND_RIGHT];
-	const Vector4& re = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_RIGHT];
-	const Vector4& rs = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_RIGHT];
-	glBegin(GL_LINES);
-		glColor3f(1.f, 0.f, 0.f);
-		if (lh.w > 0 && le.w > 0 && ls.w > 0) {
-			glVertex3f(lh.x, lh.y, lh.z);
-			glVertex3f(le.x, le.y, le.z);
-			glVertex3f(le.x, le.y, le.z);
-			glVertex3f(ls.x, ls.y, ls.z);
-		}
-		if (rh.w > 0 && re.w > 0 && rs.w > 0) {
-			glVertex3f(rh.x, rh.y, rh.z);
-			glVertex3f(re.x, re.y, re.z);
-			glVertex3f(re.x, re.y, re.z);
-			glVertex3f(rs.x, rs.y, rs.z);
-		}
-	glEnd();
+    // Draw some arms
+    const Vector4 &lh = skeletonPosition[NUI_SKELETON_POSITION_HAND_LEFT];
+    const Vector4 &le = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_LEFT];
+    const Vector4 &ls = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_LEFT];
+    const Vector4 &rh = skeletonPosition[NUI_SKELETON_POSITION_HAND_RIGHT];
+    const Vector4 &re = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_RIGHT];
+    const Vector4 &rs = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_RIGHT];
+
+    Json::Value json;
+
+    json = fillJoint(json, "hip-center", skeletonPosition[NUI_SKELETON_POSITION_HIP_CENTER]);
+    json = fillJoint(json, "spine", skeletonPosition[NUI_SKELETON_POSITION_SPINE]);
+    json = fillJoint(json, "shoulder-center", skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_CENTER]);
+    json = fillJoint(json, "head", skeletonPosition[NUI_SKELETON_POSITION_HEAD]);
+    json = fillJoint(json, "shoulder-left", skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_LEFT]);
+    json = fillJoint(json, "elbow-left", skeletonPosition[NUI_SKELETON_POSITION_ELBOW_LEFT]);
+    json = fillJoint(json, "wrist-left", skeletonPosition[NUI_SKELETON_POSITION_WRIST_LEFT]);
+    json = fillJoint(json, "hand-left", skeletonPosition[NUI_SKELETON_POSITION_HAND_LEFT]);
+    json = fillJoint(json, "shoulder-right", skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_RIGHT]);
+    json = fillJoint(json, "elbow-right", skeletonPosition[NUI_SKELETON_POSITION_ELBOW_RIGHT]);
+    json = fillJoint(json, "wrist-right", skeletonPosition[NUI_SKELETON_POSITION_WRIST_RIGHT]);
+    json = fillJoint(json, "hand-right", skeletonPosition[NUI_SKELETON_POSITION_HAND_RIGHT]);
+    json = fillJoint(json, "hip-left", skeletonPosition[NUI_SKELETON_POSITION_HIP_LEFT]);
+    json = fillJoint(json, "knee-left", skeletonPosition[NUI_SKELETON_POSITION_KNEE_LEFT]);
+    json = fillJoint(json, "ankle-left", skeletonPosition[NUI_SKELETON_POSITION_ANKLE_LEFT]);
+    json = fillJoint(json, "foot-left", skeletonPosition[NUI_SKELETON_POSITION_FOOT_LEFT]);
+    json = fillJoint(json, "hip-right", skeletonPosition[NUI_SKELETON_POSITION_HIP_RIGHT]);
+    json = fillJoint(json, "knee-right", skeletonPosition[NUI_SKELETON_POSITION_KNEE_RIGHT]);
+    json = fillJoint(json, "ankle-right", skeletonPosition[NUI_SKELETON_POSITION_ANKLE_RIGHT]);
+    json = fillJoint(json, "foot-right", skeletonPosition[NUI_SKELETON_POSITION_FOOT_RIGHT]);
+
+    jsonData =  json.toStyledString();
+    kinectWebsocketPointer->setJson(&jsonData);
+
+    glBegin(GL_LINES);
+    glColor3f(1.f, 0.f, 0.f);
+    /*if (lh.w > 0 && le.w > 0 && ls.w > 0) {
+        glVertex3f(lh.x, lh.y, lh.z);
+        glVertex3f(le.x, le.y, le.z);
+        glVertex3f(le.x, le.y, le.z);
+        glVertex3f(ls.x, ls.y, ls.z);
+    }
+    if (rh.w > 0 && re.w > 0 && rs.w > 0) {
+        glVertex3f(rh.x, rh.y, rh.z);
+        glVertex3f(re.x, re.y, re.z);
+        glVertex3f(re.x, re.y, re.z);
+        glVertex3f(rs.x, rs.y, rs.z);
+    }*/
+    for (size_t i = 0; i < NUI_SKELETON_POSITION_COUNT; i++) {
+        glVertex3f(skeletonPosition[i].x, skeletonPosition[i].y, skeletonPosition[i].z);
+    }
+    glEnd();
 }
 
-int main(int argc, char* argv[]) {
+void runWebserver(){
+    kinect_websocket kinectWebsocket;
+    kinectWebsocketPointer = &kinectWebsocket;
+    kinectWebsocket.run();
+}
+
+
+int main(int argc, char *argv[]) {
     if (!init(argc, argv)) return 1;
     if (!initKinect()) return 1;
 
+    thread t1(runWebserver);
+
     // OpenGL setup
-    glClearColor(0,0,0,0);
+    glClearColor(0, 0, 0, 0);
     glClearDepth(1.0f);
 
-	// Set up array buffers
-	const int dataSize = width*height * 3 * 4;
-	glGenBuffers(1, &vboId);
-	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
-	glGenBuffers(1, &cboId);
-	glBindBuffer(GL_ARRAY_BUFFER, cboId);
-	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
+    // Set up array buffers
+    const int dataSize = width * height * 3 * 4;
+    glGenBuffers(1, &vboId);
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
+    glGenBuffers(1, &cboId);
+    glBindBuffer(GL_ARRAY_BUFFER, cboId);
+    glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
 
     // Camera setup
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-	gluPerspective(45, width /(GLdouble) height, 0.1, 1000);
+    gluPerspective(45, width / (GLdouble) height, 0.1, 1000);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-	gluLookAt(0,0,0,0,0,1,0,1,0);
+    gluLookAt(0, 0, 0, 0, 0, 1, 0, 1, 0);
+
 
     // Main loop
     execute();
